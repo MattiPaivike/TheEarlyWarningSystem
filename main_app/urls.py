feb5bfc0a7dd5f13f7ed78c91af8e12907d883f4
Ace <earlywarningsyste@gmail.com>
2019-05-04 15:47:24 +0300

    added routers..

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,9 @@ from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register('versions', views.SoftwareApi)
+
 urlpatterns = [
     path('', views.home, name='app-home'),
     path('about/', views.about, name='app-about'),
